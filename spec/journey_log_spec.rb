1282150db13dfc3968e512559727ff2ba9763d34
ercekal <ercekalabalikoglu@gmail.com>
2016-08-26 13:03:50 +0100

    JourneyLog class is created and rspec test for start method is written

---
+++ spec/journey_log_spec.rb
@@ -0,0 +1,17 @@
+require 'journey_log'
+
+describe JourneyLog do
+
+  subject(:log) {described_class.new(journey_class: journey_class)}
+  let(:journey_class) {double :journey_class, new: journey}
+  let(:journey) {double :journey}
+  let(:entry_station) {double :station}
+  let(:exit_station) {double :station}
+
+  context '#start' do
+    it 'should create a new journey' do
+      expect(log.start(entry_station)).to eq journey
+    end
+  end
+
+end
